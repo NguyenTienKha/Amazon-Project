600c9dd1c3df3cfc0d7f091cc8b5cc89d38d6280
NguyenTienKha <nguyentienkha2908@gmail.com>
2023-08-22 00:42:54 +0700

    Signed-off-by: NguyenTienKha <nguyentienkha2908@gmail.com>

--- Amazon Project/scripts/amazon.js
+++ Amazon Project/scripts/amazon.js
@@ -1,6 +1,5 @@
-import {cart} from '../data/cart.js';
-import {products} from '../data/products.js';
-
+import { cart ,addToCart} from "../data/cart.js";
+import { products } from "../data/products.js";
 
 let displayProducts = "";
 
@@ -47,32 +46,24 @@ products.forEach((product) => {
 
 document.querySelector(".wrapper-products").innerHTML = displayProducts;
 
+
+
+
+const updateCartQuantity = () => {
+    let cartQuantity = 0;
+    cart.forEach((item) => {
+      cartQuantity += item.quantity;
+    });
+
+    document.querySelector(".js-cart-quantity").innerHTML = cartQuantity;
+};
+
 document.querySelectorAll(".js-add-to-cart").forEach((button) => {
   button.addEventListener("click", () => {
     const productId = button.dataset.productId;
 
-    let matchingItem;
-    cart.forEach((item) => {
-      if (productId === item.productId) {
-        matchingItem = item;
-      }
-    });
-
-    if (matchingItem) {
-      matchingItem.quantity += 1;
-    } else {
-      cart.push({
-        productId: productId,
-        quantity: 1,
-      });
-    }
-
-    let cartQuantity = 0;
-    cart.forEach((item)=>{
-        cartQuantity += item.quantity;
-    });
-
-    document.querySelector('.js-cart-quantity').innerHTML  = cartQuantity;
-
+    addToCart(productId);
+    updateCartQuantity();
+    
   });
 });
